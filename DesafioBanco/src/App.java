4c4ff175ed8bc6a4e2cae7db963ffd5c059d2929
Isaque <andradeisq1.7@gmail.com>
2024-10-13 04:08:27 -0300

    adicionada funcionalidade de menu com switch case para conseguir cadastrar os clientes e consultar quais os clientes cadastrados

--- DesafioBanco/src/App.java
+++ DesafioBanco/src/App.java
@@ -1,18 +1,59 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Cliente cadastrado = new Cliente();
-        cadastrado.setNome("Isaque");
+        Scanner scanner = new Scanner(System.in);
+        Banco banco = new Banco();
+        boolean continuar = true;
 
+        while (continuar) {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Cadastrar cliente e criar contas");
+            System.out.println("2. Consultar clientes cadastrados");
+            System.out.println("3. Sair");
+            System.out.print("Opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); 
 
-        Conta cc = new ContaCorrente(cadastrado);
-        cc.depositar(100);
-        cc.imprimirExtrato();
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome do cliente: ");
+                    String nomeCliente = scanner.nextLine();
+                    
+                    Cliente cadastrado = new Cliente();
+                    cadastrado.setNome(nomeCliente);
 
-        Conta poupanca = new ContaPoupanca(cadastrado);
+                    banco.adicionarCliente(cadastrado); 
+                    
+                    Conta cc = new ContaCorrente(cadastrado);
+                    cc.depositar(100); 
+                    banco.getContas().add(cc); 
 
-        cc.transferir(100, poupanca);
+                    
+                    Conta poupanca = new ContaPoupanca(cadastrado);
+                    banco.getContas().add(poupanca); 
 
-        cc.imprimirExtrato();
-        poupanca.imprimirExtrato();
+                    System.out.println("Cliente e contas cadastrados com sucesso!");
+                    break;
+
+                case 2:
+                    System.out.println("\nClientes cadastrados:");
+                    for (String nome : banco.getNomesClientes()) {
+                        System.out.println(nome);
+                    }
+                    break;
+
+                case 3:
+                    continuar = false;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+
+        scanner.close();
+        System.out.println("Programa encerrado.");
     }
 }
--- DesafioBanco/src/Banco.java
+++ DesafioBanco/src/Banco.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
-    
     private String nome;
     private List<Conta> contas;
+    private List<Cliente> clientes;
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+        this.clientes = new ArrayList<>();
+    }
 
     public String getNome() {
         return nome;
@@ -19,5 +25,17 @@ public class Banco {
 
     public void setContas(List<Conta> contas) {
         this.contas = contas;
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public List<String> getNomesClientes() {
+        List<String> nomes = new ArrayList<>();
+        for (Cliente cliente : clientes) {
+            nomes.add(cliente.getNome());
+        }
+        return nomes;
     }
 }
