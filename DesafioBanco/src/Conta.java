4da81693670369c9b56eb940a576eb38cc6f86bf
Isaque <andradeisq1.7@gmail.com>
2024-10-13 01:55:14 -0300

    implementacao de polimorfismo

--- DesafioBanco/src/Conta.java
+++ DesafioBanco/src/Conta.java
@@ -1,8 +1,16 @@
 public abstract class Conta implements IConta {
     
-    private int agencia;
-    private int numero;
-    private double saldo;
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     public int getAgencia() {
         return agencia;
--- DesafioBanco/src/ContaCorrente.java
+++ DesafioBanco/src/ContaCorrente.java
@@ -1,4 +1,4 @@
 public class ContaCorrente extends Conta {
-    
+
     
 }
