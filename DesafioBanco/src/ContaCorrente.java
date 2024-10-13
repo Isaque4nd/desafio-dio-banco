891ec409dcae57783435e9e3cbe1866d8e83d5ec
Isaque <andradeisq1.7@gmail.com>
2024-10-13 01:40:34 -0300

    adição de classes e interfaces utilizando herança

--- DesafioBanco/src/ContaCorrente.java
+++ DesafioBanco/src/ContaCorrente.java
@@ -1,30 +1,4 @@
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
     
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void sacar() {
-
-    }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-
-    }
+    
 }
